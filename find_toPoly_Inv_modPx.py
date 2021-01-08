646471af273866ba75e16d70ef574e7507eaa0ed
tokatm <mstf.tokat@gmail.com>
2021-01-08 18:50:28 +0300

    find 2 poly

---
+++ find_toPoly_Inv_modPx.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+m = input("değer gir: ").split(',')
+#s = input("ikinci gir: ").split(',')
+
+x = np.poly1d(list(map(int, m)))
+#y = np.poly1d(list(map(int, s)))
+p = np.poly1d([1,0,0,0,1,1,0,1,1])
+
+
+def inv(x,p):
+   k = np.polydiv(x,p)
+   if 
+
+inv(x,y)
+
+
+#%%
+'''
+   #res = np.polydiv(k,p)
+   quotient, remainder = np.polydiv(k,p)
+   list_e = (remainder.coefficients % 2)
+   print(np.poly1d(list_e)) 
