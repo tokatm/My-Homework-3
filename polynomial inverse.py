06bb8ce26110c8506cdf73df89ec7fe1a731699d
tokatm <mstf.tokat@gmail.com>
2021-01-08 18:09:47 +0300

    kullanııdan veri girişi istendi

--- polynomial inverse.py
+++ polynomial inverse.py
@@ -1,10 +1,15 @@
 import numpy as np
 #import numpy.polymul as npp
 
+m = input("değer gir: ").split(',')
+s = input("ikinci gir: ").split(',')
 
-x = np.poly1d([1,0,1,0,0,1,0,1])
-y = np.poly1d([0,0,0,0,1,1,0,1])
+x = np.poly1d(list(map(int, m)))
+y = np.poly1d(list(map(int, s)))
+#x = np.poly1d([1,0,1,0,0,1,0,1])
+#y = np.poly1d([0,0,0,0,1,1,0,1])
 p = np.poly1d([1,0,0,0,1,1,0,1,1])
+#print(x,y)
 
 def inv(x,y):
    k = np.polymul(x,y)
